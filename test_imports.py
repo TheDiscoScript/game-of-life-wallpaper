6b8d0113c443b5cd1ac9cf30d4298b17e24e9e40
TheDiscoScript <wollmada@gmail.com>
2024-07-13 18:01:21 +0200

    chore: removed forgotten testing file

--- test_imports.py
+++
@@ -1,7 +0,0 @@
-# test_imports.py
-try:
-    from PIL import Image
-    from AppKit import NSWorkspace, NSScreen
-    print("Imports successful!")
-except ImportError as e:
-    print(f"Import error: {e}")
